8274dfbde291acb124244c02d03d6c2af8242fa6
Billy Zedan <billyzedan123@gmail.com>
2020-12-04 19:12:31 -0800

    Initial Commit

---
+++ StockApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StockApp
+//
+//  Created by Field Employee on 12/4/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
